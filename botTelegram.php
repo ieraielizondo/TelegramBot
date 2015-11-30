ecd42665ba4def3b06beea9267b3185fdc17dfbe
ieraielizondo <ieraielizondo@gmail.com>
2015-11-30 02:42:39 +0100

    a
    a

--- botTelegram.php
+++ botTelegram.php
@@ -10,19 +10,22 @@ $updateArray=json_decode($update,true);
 //print_r($update);
 
 $cont=count($updateArray['result'])-1;
-$name=$updateArray['result'][$cont]['message']['from']['first_name'];
-if(isset($updateArray['result'][$cont]['message']['from']['last_name'])){
-	$name.=" ".$updateArray['result'][$cont]['message']['from']['last_name'];
-}
-$chatId=$updateArray["result"][$cont]['message']['chat']['id'];
+if(count($updateArray['result'])>0){
+	$name=$updateArray['result'][$cont]['message']['from']['first_name'];
+	if(isset($updateArray['result'][$cont]['message']['from']['last_name'])){
+		$name.=" ".$updateArray['result'][$cont]['message']['from']['last_name'];
+	}
+	$chatId=$updateArray["result"][$cont]['message']['chat']['id'];
 
-$text=$updateArray["result"][$cont]['message']['text'];
+	$text=$updateArray["result"][$cont]['message']['text'];
+}
+
 if($text==="/kaixo"){
 	$mensaje="Kaixo, soy un bot creado por Ierai";
 }else if($text==="/quiensoy"){
 	$mensaje="Eres...".$name." ¿He acertado?";
 }else{
-	$mensaje="No entiendo tu mensaje, pero puedes visitar https://github.com/ieraielizondo para ver el código. :)";
+	$mensaje="No entiendo tu mensaje, pero puedes visitar https://github.com/ieraielizondo para ver el código."."f0 9f 91 8d";
 }
 
 sendMessage($chatId,$mensaje);
